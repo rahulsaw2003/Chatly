47b7fc7d417aa0f9b5c9ffb8a8be2941f702fb94
Rahul Kumar Saw <2021chb1052@iitrpr.ac.in>
2023-11-06 18:53:52 +0530

    updated server url

--- client/src/SocketContext.jsx
+++ client/src/SocketContext.jsx
@@ -4,7 +4,7 @@ import Peer from "simple-peer";
 
 const SocketContext = createContext();
 
-const socket = io("http://localhost:8000");
+const socket = io(process.env.REACT_APP_SERVER_URL);
 
 const ContextProvider = ({ children }) => {
 	const [callAccepted, setCallAccepted] = useState(false);
